d5ce2ed00df44edf9c55c1a60bde7b686d74ff80
firas hamila <firas.hm.1996@gmail.com>
2025-01-14 12:37:16 +0100

    fixed0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-function App() {
+import React from "react";
+
+const App = () => {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2>aaaaaaaa</h2>
     </div>
   );
-}
+};
 
 export default App;
