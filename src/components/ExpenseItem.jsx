2a2288d9dac64216023a729c1264a0851284a0be
firas hamila <firas.hm.1996@gmail.com>
2025-01-20 20:09:40 +0100

    a

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,10 +1,11 @@
+import "./ExpenseItem.css";
 const ExpenseItem = () => {
   return (
-    <div>
+    <div className="expense-item">
       <div>date</div>
-      <div>
+      <div className="expense-item__description">
         <h3>title</h3>
-        <div>price</div>
+        <div className="expense-item__price">price</div>
       </div>
     </div>
   );
