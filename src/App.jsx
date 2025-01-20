32a2af39b680485b915c7d9c249847b76d01f03b
firas hamila <firas.hm.1996@gmail.com>
2025-01-20 19:34:53 +0100

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
-      <h1>Lets get started !!!</h1>
+      <ExpenseItem />
     </div>
   );
 }
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,13 @@
+const ExpenseItem = () => {
+  return (
+    <div>
+      <div>date</div>
+      <div>
+        <h3>title</h3>
+        <div>price</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
