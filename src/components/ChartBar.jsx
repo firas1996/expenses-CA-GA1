b0fb3b528eefc169a451ee83b49c72472670c22b
firas hamila <firas.hm.1996@gmail.com>
2025-01-27 19:24:21 +0100

    a

---
+++ src/components/ChartBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ChartBar.css";
+
+const ChartBar = () => {
+  return (
+    <div>
+      <div>
+        <div></div>
+      </div>
+      <div>Jan</div>
+    </div>
+  );
+};
+
+export default ChartBar;
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
   const year = props.date.getFullYear();
   const month = props.date.toLocaleString("ar-TN", { month: "long" });
   const day = props.date.toLocaleString("ar-TN", { day: "2-digit" });
+  const [title, setTitle] = useState(props.title);
+  const titleChangeHandler = () => {
+    setTitle("New Title !");
+    console.log(title);
+  };
   return (
     <div className="expense-item">
       <div className="expense-date">
@@ -11,7 +17,8 @@ const ExpenseItem = (props) => {
         <div className="expense-date__day">{day}</div>
       </div>
       <div className="expense-item__description">
-        <h3>{props.title}</h3>
+        <h3>{title}</h3>
+        <button onClick={titleChangeHandler}>Update Title !!!</button>
         <div className="expense-item__price">$ {props.price}</div>
       </div>
     </div>
