df2670c0b8c8bd30aa71fd44010682faea46ef29
firas hamila <firas.hm.1996@gmail.com>
2025-02-03 19:07:59 +0100

    a

---
+++ src/components/Filter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Filter.css";
+const Filter = () => {
+  return (
+    <div className="expenses-filter">
+      <label>Filter By Year</label>
+      <select>
+        <option>a</option>
+        <option>b</option>
+        <option>c</option>
+      </select>
+    </div>
+  );
+};
+
+export default Filter;
