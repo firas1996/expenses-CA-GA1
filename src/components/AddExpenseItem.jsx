476beb7e67650f36547c9958dd75697a2f50ac56
firas hamila <firas.hm.1996@gmail.com>
2025-02-10 19:01:47 +0100

    aa

--- src/components/AddExpenseItem.jsx
+++ src/components/AddExpenseItem.jsx
@@ -1,25 +1,29 @@
 import "./AddExpenseItem.css";
 
 const AddExpenseItem = () => {
+  const dateSys = new Date(Date.now()).getFullYear();
+  console.log(dateSys);
+  const maxDate = `${dateSys + 2}-12-31`;
+  const minDate = `${dateSys - 2}-01-01`;
   return (
     <div className="new-expense">
       <form>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input />
+            <input placeholder="Title" />
           </div>
           <div className="new-expense__control">
             <label>Price</label>
-            <input />
+            <input placeholder="Price" type="number" min="0" />
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input />
+            <input type="date" min={minDate} max={maxDate} />
           </div>
         </div>
         <div className="new-expense__actions">
-          <button>Concel</button>
+          <button>Cancel</button>
           <button>Add Expense</button>
         </div>
       </form>
